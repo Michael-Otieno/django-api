44d7f5ff82e47a8b7609ba794a5a2a59749f3ad1
Michael-Otieno <michael.otieno@student.moringaschool.com>
2022-12-23 11:15:21 +0300

    migrate land details

--- shamba/models.py
+++ shamba/models.py
@@ -1,7 +1,30 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
-import uuid
+from django.contrib.auth.models import AbstractUser,BaseUserManager
+# from django.utils.translation import ugettext_lazy as _
 # Create your models here.
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError('The Email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+        return self.create_user(email, password, **extra_fields)
+
+
 class User(AbstractUser):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
@@ -9,10 +32,13 @@ class User(AbstractUser):
     phone_number = models.CharField(max_length=255,null=True,default=None)
     email = models.CharField(max_length=255, unique=True)
     password = models.CharField(max_length=255)
+    username = None
+
+    objects = CustomUserManager()
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
-
+    
 
 class LandOwner(models.Model):
     ProofOwnershipDocument = models.FileField(db_column='image_url', blank=True, null=True, upload_to='images/')
@@ -28,7 +54,7 @@ class LandOwner(models.Model):
     UserId = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.UserId
+        return self.NextOfKin
 
 
 class LandInformation(models.Model):
@@ -43,6 +69,6 @@ class LandInformation(models.Model):
     LandDes = models.TextField(max_length=255)
 
     def __str__(self):
-        return self.LandOwner
+        return self.LandDes
 
 
